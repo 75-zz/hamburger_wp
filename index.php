85bb5164446e3eaeb811f2f02c57ac286c50f14b
75-zz <986007+75-zz@users.noreply.github.com>
2021-06-08 20:41:37 +0900

    ndexphp修正、検索結果を表示可能

--- index.php
+++ index.php
@@ -1,55 +1,46 @@
 <?php get_header(); ?>
-<div class="l-hero">
-    <h2 class="p-hero__title">ダミーサイト</h2>
-</div>
-<section class="p-branch-contents">
 
-
-    <div class="p-branch-wrapper">
-    <a href="<?php echo get_page_link(64); ?>">
-    <article class="c-branch__card p-takeout">
-            <h3>Take Out</h3>
-
-            <div class="c-branch__textarea">
-                <dl>
-                    <dt>小見出しが入ります</dt>
-                    <dd>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                    </dd>
-                </dl>
-                <dl>
-                    <dt>小見出しが入ります</dt>
-                    <dd>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                    </dd>
-                </dl>
-            </div>
-            </article></a>
-            <a href="<?php echo get_page_link(66); ?>">
-        <article class="c-branch__card p-eatin">
-            <h3>Eat In</h3>
-            <div class="c-branch__textarea">
-                <dl>
-                    <dt>小見出しが入ります</dt>
-                    <dd>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                    </dd>
-                </dl>
-                <dl>
-                    <dt>小見出しが入ります</dt>
-                    <dd>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                    </dd>
-                </dl>
-            </div>
-        </article></a>
+                <div class="l-hero-archive">
+                    <h1 class="p-hero-archive__title"><?php the_search_query(); ?></h1>
+                </div>
+                <section class="p-content-archive">
+                    <h2>小見出しが入ります</h2>
+                    <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+                    
+                   
+                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <article class="c-cards">
+                <?php if (has_post_thumbnail()) : ?>
+                    <img class="blog-item__thumbnail-image" src="<?php the_post_thumbnail_url('large'); ?>">
+                <?php endif; ?>
+                <div class="c-cards__textarea">
+                    <h3><?php the_title(); ?></h3>
+                    <h4><?php the_excerpt(); ?></h4>
+                    <p><?php the_content('', FALSE, ''); ?></p>
+                    <button><a href="<?php the_permalink(); ?>">詳しく見る</a> </button>
+                </div>
+            </article>
+    <?php endwhile; ?>
+    
+    <?php else: ?>
+    <div class="col-12 text-center">
+    <p>検索結果はありませんでした</p>
     </div>
+<?php endif; ?>
+
+                    </section>
+                    <section class="c-pagination">
+        <?php
+        $args = array(
+            'mid_size' => 6,
+            'prev_text' => '&lt;&lt;',
+            'next_text' => '&gt;&gt;',
+            'screen_reader_text' => ' ',
+        );
+        the_posts_pagination($args);
+        ?>
 </section>
-<section class="p-map">
-    <article class="p-map-direction">
-        <h3>見出しが入ります</h3>
-        <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-        </p>
-    </article>
-    <img src="<?php echo get_template_directory_uri(); ?>/img/map.png' ?>" alt="周辺MAP">
-</section>
-</main>
-<?php get_sidebar(); ?>
+            </main>
+            <?php get_sidebar(); ?>
 </div>
 <?php get_footer(); ?>
